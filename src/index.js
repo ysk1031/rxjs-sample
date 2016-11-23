a373c05085359f4a131493e261131a6b39255dcd
ysk1031 <yusukeaono.1031@gmail.com>
2016-11-23 23:01:01 +0900

    Implement sample (github-users suggestion like twitter)
    http://ninjinkun.hatenablog.com/entry/introrxja
    上記ページのサンプルコードを実装

--- src/index.js
+++ src/index.js
@@ -1,44 +1,61 @@
 import $ from 'jquery';
 import Rx from 'rx';
 
-const inputStream = Rx.Observable.fromEvent($('#query'), 'input')
-  .map(e => e.target.value);
+const refreshClickStream = Rx.Observable.fromEvent($('.refresh'), 'click');
 
-// const queryStream = inputStream
-//   .filter(text => text.length > 0)
-//   .map(text => `https://api.github.com/search/repositories?q=${text}`);
-
-const queryStream = inputStream
-  .throttle(300)
-  .filter(text => text.length > 0)
-  .distinctUntilChanged()
-  .map(text => `https://api.github.com/search/repositories?q=${text}`);
-
-// const query = `https://api.github.com/search/repositories?q=${text}`;
-// Rx.Observable.fromPromise($.ajax({url: query}));
-
-const repositoriesStream = queryStream
-  .flatMap((query) => {
-    const promise = $.ajax({url: query});
-    return Rx.Observable.fromPromise(promise);
-  })
-  .map(res => res.items);
-
-const htmlStream = repositoriesStream
-  .map((items) => {
-    return items.map((repo) => {
-      const $p = $('<p>').append(
-        $('<a>').attr({href: repo.html_url}).append(repo.description)
-      );
-      return $p[0].outerHTML;
-    });
+const requestStream = refreshClickStream.startWith('startup click')
+  .map(() => {
+    const randomOffset = Math.floor(Math.random() * 500);
+    return `https://api.github.com/users?since=${randomOffset}`;
   });
 
-htmlStream.subscribe((htmlParts) => {
-  $('#search-result').empty();
-  const $table = $('<table>');
-  $.each(htmlParts, (_, html) => {
-    $table.append($('<tr>').append($('<td>').append(html)));
-  });
-  $('#search-result').append($table);
+const responseStream = requestStream
+  .flatMap(requestUrl => Rx.Observable.fromPromise($.getJSON(requestUrl)));
+
+const close1ClickStream = streamByCloseClick('.close1');
+const close2ClickStream = streamByCloseClick('.close2');
+const close3ClickStream = streamByCloseClick('.close3');
+
+const suggestion1Stream = suggestionStream(close1ClickStream);
+const suggestion2Stream = suggestionStream(close2ClickStream);
+const suggestion3Stream = suggestionStream(close3ClickStream);
+
+suggestion1Stream.subscribe(suggestedUser => {
+  renderSuggestion(suggestedUser, '.suggestion1');
 });
+suggestion2Stream.subscribe(suggestedUser => {
+  renderSuggestion(suggestedUser, '.suggestion2');
+});
+suggestion3Stream.subscribe(suggestedUser => {
+  renderSuggestion(suggestedUser, '.suggestion3');
+});
+
+
+function streamByCloseClick(selector) {
+  return Rx.Observable.fromEvent($(selector), 'click');
+}
+
+function suggestionStream(closeClickStream) {
+  return closeClickStream.startWith('startup click')
+    .combineLatest(responseStream, (_, listUsers) => {
+      return listUsers[Math.floor(Math.random() * listUsers.length)];
+    })
+    .merge(refreshClickStream.map(() => null))
+    .startWith(null);
+}
+
+function renderSuggestion(suggestedUser, selector) {
+  const suggesttionEl = document.querySelector(selector);
+  if (suggestedUser == null) {
+    suggesttionEl.style.visibility = 'hidden';
+  } else {
+    suggesttionEl.style.visibility = 'visible';
+
+    const userNameEl = suggesttionEl.querySelector('.username');
+    userNameEl.href = suggestedUser.html_url;
+    userNameEl.textContent = suggestedUser.login;
+    const imgEl = suggesttionEl.querySelector('img');
+    imgEl.src = "";
+    imgEl.src = suggestedUser.avatar_url;
+  }
+}
